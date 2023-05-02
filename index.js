1fb0d8b8f7eafcff06632aa826a51452956c7dad
Salih Ekici <r0883622@student.thomasmore.be>
2023-05-02 15:22:02 +0200

    update

--- index.js
+++ index.js
@@ -22,3 +22,33 @@ appearOptions);
 faders.forEach((fader) => {
   appearOnScroll.observe(fader);
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btns = document.getElementsByClassName("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+Array.from(btns).forEach(function (btn) {
+  // code to execute on each button
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+});
+
+// When the user clicks on the button, open the modal
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
