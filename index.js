72f4d6012b04dd8ff50dcb844dd9da600ad39395
Salih Ekici <r0883622@student.thomasmore.be>
2024-06-11 15:19:56 +0200

    added carousels

--- index.js
+++ index.js
@@ -27,29 +27,46 @@ document.addEventListener('DOMContentLoaded', function () {
   fetch('./src/data/projects.json')
     .then(response => response.json())
     .then(data => {
-      const generateContributionsHTML = (contributions) => {
-        return contributions.map(contribution => `<li>${contribution}</li>`).join('');
-      };
-
       const modal = document.getElementById("myModal");
       const span = document.getElementsByClassName("close")[0];
+      const carousel = this.documentElement.getElementsByClassName("carousel-inner")[0];
       const projectTitle = document.getElementById("projectName");
       const projectBackground = document.getElementById("projectBackground");
       const technologies = document.getElementById("technologies");
       const contributions = document.getElementById("contributions");
       const remarks = document.getElementById("remarks");
-
+      const generateContributionsHTML = (contributions) => {
+        return contributions.map(contribution => `<li>${contribution}</li>`).join('');
+      };
+      const generateCarouselImages = (images) => {
+        const carouselItems = [];
+        images.forEach((image, index) => {
+          if (index === 0) {
+            carouselItems.push(`<div class="carousel-item active" >
+                    <img class="d-block w-100" src="${image}">
+                </div>`);
+          } else {
+            carouselItems.push(`<div class="carousel-item" >
+                  <img class="d-block w-100" src="${image}" style="height: 100%;">
+              </div>`);
+          }
+        });
+        return carouselItems.join('');
+      }
       const openModal = (project) => {
+        carousel.innerHTML = "";
         modal.style.display = "block";
         projectTitle.innerHTML = project.title;
         projectBackground.innerHTML = project.background;
         technologies.innerHTML = project.technologies;
+        carousel.innerHTML = generateCarouselImages(project.images);
+        console.log(carousel.innerHTML);
         contributions.innerHTML = `<ul>${generateContributionsHTML(project.contributions)}</ul>`;
         remarks.innerHTML = project.remarks;
       };
 
       data.forEach(project => {
-        const btn = document.getElementById(project.id);
+        const btn = document.getElementById(project.btnid);
         if (btn) {
           btn.onclick = () => openModal(project);
         }
@@ -57,6 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       span.onclick = function () {
         modal.style.display = "none";
+        
       };
 
       window.onclick = function (event) {
